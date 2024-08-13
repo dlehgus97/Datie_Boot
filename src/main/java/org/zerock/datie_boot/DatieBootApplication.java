716f54f10b888a309f734a2def7e506cc8fd9fd6
이진우 <dlehgus97@naver.com>
2024-08-13 15:42:37 +0900

    first commit

---
+++ src/main/java/org/zerock/datie_boot/DatieBootApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.datie_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatieBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatieBootApplication.class, args);
+	}
+
+}
