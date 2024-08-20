f96506e4e2b0c18b72d4aaa4110454894c956698
hanjucoding <tony990205@naver.com>
2024-08-20 11:54:22 +0900

    Card,User엔티티생성

--- src/main/java/org/zerock/datie_boot/controller/CompanyController.java
+++ src/main/java/org/zerock/datie_boot/controller/CompanyController.java
@@ -2,10 +2,7 @@ package org.zerock.datie_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.datie_boot.entity.Company;
 import org.zerock.datie_boot.repository.CompanyRepository;
 
@@ -16,15 +13,13 @@ public class CompanyController {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @CrossOrigin(origins = "http://localhost:3000") // React 개발 서버의 주소
     @GetMapping("/company")
     public ResponseEntity<Company> getCompanyById(@RequestParam("companyno") int companyno) {
-        System.out.println("Received request for company with companyno: " + companyno);
         Company company = companyRepository.findById(companyno).orElse(null);
         if (company == null) {
-            System.out.println("Company not found with companyno: " + companyno);
             return ResponseEntity.notFound().build();
         }
-        System.out.println("Company found: " + company);
         return ResponseEntity.ok(company);
     }
 }
