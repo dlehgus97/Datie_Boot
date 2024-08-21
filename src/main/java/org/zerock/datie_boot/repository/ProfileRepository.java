70111f4fcf2c512c9708d34af2ac30ac9af01142
이진우 <dlehgus97@naver.com>
2024-08-21 15:07:57 +0900

    회원번호로 조회

---
+++ src/main/java/org/zerock/datie_boot/repository/ProfileRepository.java
@@ -0,0 +1,17 @@
+package org.zerock.datie_boot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.zerock.datie_boot.dto.ProfileDTO;
+import org.zerock.datie_boot.entity.User;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT new org.zerock.datie_boot.dto.ProfileDTO(u, a.bank, a.account) " +
+            "FROM User u LEFT JOIN Account a ON u.accountno = a.accountno " +
+            "WHERE u.userno = :userno")
+    Optional<ProfileDTO> findUserWithAccountByUserno(@Param("userno") int userno);
+}
