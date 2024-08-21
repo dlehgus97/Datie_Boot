56bf09a47a777d518b5fc85360da7ad691d65053
kimgo <kimgo816@naver.com>
2024-08-21 09:38:11 +0900

    Diary DTO, Repository, Service 추가

--- src/main/java/org/zerock/datie_boot/service/DiaryService.java
+++ src/main/java/org/zerock/datie_boot/service/DiaryService.java
@@ -2,6 +2,7 @@ package org.zerock.datie_boot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.zerock.datie_boot.repository.DiaryRepository;
 
 import java.util.List;
 
