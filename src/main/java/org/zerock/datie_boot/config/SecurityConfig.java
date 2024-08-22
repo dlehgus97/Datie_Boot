aa49575c95cc6df9b6bb65140737460345389546
양재혁 <yjh5128@naver.com>
2024-08-22 10:43:34 +0900

    240822 gpt 기능 추가 및 security 허용

--- src/main/java/org/zerock/datie_boot/config/SecurityConfig.java
+++ src/main/java/org/zerock/datie_boot/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/profile").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/admin/list").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/admin/list").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/chat/request").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(auth -> auth.disable())
