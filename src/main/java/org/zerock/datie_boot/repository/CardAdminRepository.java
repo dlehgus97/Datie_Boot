88b9765b11bf2d3ef6a4a0a0c70a7284084be135
ohhyeonn <ohhyeon120@gmail.com>
2024-08-26 17:31:40 +0900

    어드민 수정반영 서현오

--- src/main/java/org/zerock/datie_boot/repository/CardAdminRepository.java
+++ src/main/java/org/zerock/datie_boot/repository/CardAdminRepository.java
@@ -6,13 +6,29 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.datie_boot.entity.Card;
 
+import java.sql.Timestamp;
+
 public interface CardAdminRepository extends JpaRepository<Card, Integer> {
-    @Modifying // update는 반드시 추가
-    @Query("update Card set cStatus=3 where userno=:id ")
-    public void deactivateByUserId(String id);
 
 
     @Modifying // update는 반드시 추가
-    @Query("update Card set cStatus=2 where userno=:id ")
-    public void holdByUserId(String id);
+    @Query("UPDATE Card c SET c.cStatus = 3 WHERE c.userno = :id " +
+            "AND c.cModdate = :maxDate")
+    public void deactivateByUserId(String id, Timestamp maxDate);
+
+
+    @Modifying // update는 반드시 추가
+    @Query("UPDATE Card c SET c.cStatus = 2 WHERE c.userno = :id " +
+            "AND c.cModdate = :maxDate")
+    public void holdByUserId(String id, Timestamp maxDate);
+
+
+    @Modifying // update는 반드시 추가
+    @Query("update User set cardno=0 where userno=:id ")
+    public void zeroCardno(String id);
+
+    @Query("SELECT MAX(c.cModdate) " +
+            "FROM Card c " +
+            "WHERE c.userno = :id")
+    String getMaxDate(String id);
 }
